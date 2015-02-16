1d9d42c55aeb5970e71e4447dce443297c22e5e4
JulienRouse <julien.rouse@gmail.com>
2015-02-16 19:13:47 +0100

    tada

--- FTP/src/util/Messages.java
+++ FTP/src/util/Messages.java
@@ -21,10 +21,12 @@ public class Messages {
 	public static final String USER_INVALID = "430 Invalid username or password\n";
 	
 	public static final String PASS_OK = "230 User logged in, proceed. Logged out if appropriate.\n";
-	public static final String PASS_ERROR = "530 Not logged in.\n";
+	public static final String NOT_LOGGED_IN = "530 Not logged in.\n";
 	
 	public static final String QUIT = "221\n";
-	
+        public static final String SYNTAX_ERROR = "520 \n";
+     
+
 	public static final String toto = "";		
 			
 			
