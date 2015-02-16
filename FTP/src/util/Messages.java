9e9e079daba161d42306e9ae3a29de361fc69036
JulienRouse <julien.rouse@gmail.com>
2015-02-16 21:50:34 +0100

    comm pour processsyst et ajout ds message

--- FTP/src/util/Messages.java
+++ FTP/src/util/Messages.java
@@ -23,9 +23,9 @@ public class Messages {
 	public static final String PASS_OK = "230 User logged in, proceed. Logged out if appropriate.\n";
 	public static final String NOT_LOGGED_IN = "530 Not logged in.\n";
 	
+	public static final String SUCCES = "200 The requested action has been successfully completed. \n"
 	public static final String QUIT = "221\n";
-	public static final String SUCCESS = "200 command okay\n";
-    public static final String SYNTAX_ERROR = "520 \n";
+        public static final String SYNTAX_ERROR = "520 \n";
      
 
 	public static final String toto = "";		
