21449b1846b501e4467b9fa3be1976fcf0748f74
Tyboon <emilie.tyboon@gmail.com>
2015-02-18 15:04:35 +0100

    serveur

--- FTP/src/server/Serveur.java
+++ FTP/src/server/Serveur.java
@@ -33,19 +33,7 @@ public class Serveur {
 
 	}
 	
-	public static void main(String[] args) throws IOException {
-		Serveur serveur = new Serveur(4000);
-		//Boolean active = true;
-		System.out.println("Demarrage du serveur");
-		System.out.println("En attente de connexion\n");
-		while (true) {
-			Socket connexion = serveur.socket.accept();
-			FtpRequest requete = new FtpRequest(connexion, serveur.map_user);
-			requete.start();
-			System.out.println("nouveau client ");
-		}
 
-	}
 	/**
 	 * Lit dans le fichier contenant les utilisateurs autorisé, et les
 	 * chargent dans une HashMap 
