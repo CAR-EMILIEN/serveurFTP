98ce171025eeea3b6ee50862d9871c1b892af6c6
JulienRouse <julien.rouse@gmail.com>
2015-02-18 15:22:17 +0100

    arg

--- FTP/src/server/Main.java
+++ FTP/src/server/Main.java
@@ -1,8 +1,15 @@
 package server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
-
+/**
+ * 
+ * 
+ * 
+ * @author rouse & allart
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
@@ -10,8 +17,16 @@ public class Main {
 			System.out.println("usage: server path_server");
 			System.exit(1);
 		}
+		else {
+			File f = new File(args[0]);
+			if (!f.isDirectory())
+			{
+				System.out.println( args[0]);
+				System.out.println("arg invalid");
+				System.exit(1);
+			}
+		}
 		Serveur serveur = new Serveur(4000);
-		//Boolean active = true;
 		System.out.println("Demarrage du serveur");
 		System.out.println("En attente de connexion\n");
 		while (true) {
@@ -20,5 +35,6 @@ public class Main {
 			requete.start();
 			System.out.println("nouveau client ");
 		}
+		
 	}
 }
